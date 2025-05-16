b7c0bf11022863ef2d38f781dd15eddc37d2e117
RaY8118 <parthghadi43@gmail.com>
2025-05-16 10:23:11 +0530

    CORS configuration for prodcution build

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,23 +1,26 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.v1.api import api_router
 from app.db.database import Base, engine
 
+load_dotenv()
 Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Voting System with Face Recogition")
 app.include_router(api_router, prefix="/api/v1")
 
 # Configure CORS
-origins = [
-    "http://localhost:5173",  # Vite dev server
-    "http://localhost:3000",  # React dev server
-    "https://voting-system-frontend.vercel.app",  # Production frontend
-]
+allowed_origins = os.getenv("ALLOWED_ORIGINS")
+origins = [origins.strip()
+           for origins in allowed_origins.split(",") if origins.strip()]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
