ef42c65714ff3552f8fa5c94b639f9573c0576e5
RaY8118 <parthghadi43@gmail.com>
2025-05-18 15:06:32 +0530

    Remove build warnings

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './App.css'
+import './index.css'
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
